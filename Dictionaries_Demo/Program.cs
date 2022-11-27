456942f2e6c8ea708ce92a5af3c582a16bdd56a0
Alma <alma@alma-c-box.local>
2022-11-27 09:51:07 -0600

    Add Initial Project and Source Files

---
+++ Dictionaries_Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dictionaries_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Encrypt and decrypt a message using a Dictionary
+            // as the key/value storage system
+            List<char> letters = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 
+                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
+                't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+
+        }
+    }
+}
