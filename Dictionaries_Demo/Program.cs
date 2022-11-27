b6ced0b18935e0483b69554c2a78fd912b4ba4a7
Alma <alma@alma-c-box.local>
2022-11-27 10:53:45 -0600

    Add Code to Create a Dictionary Cipher

--- Dictionaries_Demo/Program.cs
+++ Dictionaries_Demo/Program.cs
@@ -10,7 +10,64 @@
                 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
                 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+            Dictionary<char, char> cipher = new Dictionary<char, char>();
 
+            List<bool> numberUsed = new List<bool>();
+
+            for (int i = 0; i < 26; i++)
+            {
+                numberUsed.Add(false);
+            }
+
+            Random rand = new Random();
+            int randNum;
+            int counter = 0;
+
+            // Construct our cipher dictionary using randon numbers
+            foreach (char letter in letters)
+            {
+                randNum = rand.Next(26);
+
+                while (numberUsed[randNum] == true)
+                {
+                    randNum = rand.Next(26);
+                }
+                 
+                numberUsed[randNum] = true;
+
+                cipher.Add(letters[counter], letters[randNum]);
+
+                counter++;
+            }
+
+            // Encrypt a message using the dictionary cipher
+            string? myMessage = "scissors";
+            string mySecretMessage = string.Empty;
+
+            Console.WriteLine($"The message you want to encrypt is: {myMessage}");
+            Console.WriteLine();
+
+            foreach (char letter in myMessage)
+            {
+                mySecretMessage = mySecretMessage + cipher[letter];
+            }
+
+            Console.WriteLine($"The encrypted message is: {mySecretMessage}");
+            Console.WriteLine();
+
+            // Decrypt the message using the dictionary cipher
+            Console.WriteLine($"The message you want to decrypt is: {mySecretMessage}");
+            Console.WriteLine();
+
+            myMessage = string.Empty;
+
+            foreach (char letter in mySecretMessage)
+            {
+                myMessage = myMessage + cipher.FirstOrDefault(x => x.Value == letter).Key;
+            }
+
+            Console.WriteLine($"The decrypted message is: {myMessage}");
+            Console.WriteLine();
         }
     }
 }
